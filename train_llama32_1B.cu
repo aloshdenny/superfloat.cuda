22349cc57533942ea5e78876c1f2551affe27c9d
Alosh Denny <aloshdenny@gmail.com>
2026-03-20 18:39:58 +0530

    ahh lower lr and more warmup

--- train_llama32_1B.cu
+++ train_llama32_1B.cu
@@ -1372,12 +1372,12 @@ int main(int argc, char *argv[]) {
     int num_iterations       = -1;       // -1 = auto
     int inference_only       = 0;
 #if defined(ENABLE_Q115)
-    float learning_rate      = 1e-4f;
-    int warmup_iters         = 200;
+    float learning_rate      = 5e-5f;
+    int warmup_iters         = 400;
     float grad_clip          = 0.5f;
 #else
-    float learning_rate      = 3e-4f;
-    int warmup_iters         = 0;
+    float learning_rate      = 5e-5f;
+    int warmup_iters         = 400;
     float grad_clip          = 1.0f;
 #endif
     float lr_decay_frac      = 1.0f;
